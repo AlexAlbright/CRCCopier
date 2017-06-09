58619dfae69583d557562e90e46e2665952657c3
Alex Albright <albright@alexs-imac.local>
2017-06-09 14:14:46 -0600

    Initial Commit

---
+++ CRCCopier/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CRCCopier
+//
+//  Created by Alex Albright on 6/9/17.
+//  Copyright © 2017 Alex Albright. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CRCCopier/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CRCCopier
+//
+//  Created by Alex Albright on 6/9/17.
+//  Copyright © 2017 Alex Albright. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
